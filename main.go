79b71452c8f8b4f7c6dc2c4af39f7e406f07fa5d
AmelAbema <dogormous14@gmail.com>
2023-08-07 15:40:43 +0200

    adds middleware to count requests

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileServerHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits)))
+	if err != nil {
+		return
+	}
+}
+
+func handler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		return
+	}
+}
+
 func main() {
+	cfg := apiConfig{fileServerHits: 0}
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/healthz", handler)
+	mux.HandleFunc("/metrics", cfg.handleMetrics)
 
 	corsMux := middlewareCors(mux)
 
-	server := http.Server{
+	server := &http.Server{
 		Handler: corsMux,
 		Addr:    ":8080",
 	}
